9109fb1127da40279ec4ebe92ee393561d58015f
Zana Dynasty <boomkyra2022@gmail.com>
2026-09-14 10:23:20 +0700

    fix: unclosed div in flow-prompt-director-client.tsx breaking build

--- components/flow-prompt-director-client.tsx
+++ components/flow-prompt-director-client.tsx
@@ -764,6 +764,7 @@ export default function FlowPromptDirectorClient({ products, personas, ideas, sc
                       ))}
                     </tbody>
                   </table>
+                  </div>
                 </details>
               </div>
             ))}
